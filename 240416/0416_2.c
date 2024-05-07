1512ad56908fbd43c62bfde77c43f9aae6d276bf
downpour07 <choiheeyoung10@gmail.com>
2024-05-07 12:30:14 +0900

    240507

--- 240416/0416_2.c
+++ 240416/0416_2.c
@@ -4,32 +4,40 @@
 #include <stdlib.h>
 
 int main() {
-	pid_t pid[3];
-	int status;
+    pid_t pid[3];
+    int status;
 
-	for(int i = 0; i<3; i++) {
-		pid[i] = -1;
-	}
+    for (int i = 0; i < 3; i++) {
+        pid[i] = -1;
+    }
 
-	pid[0] = fork();
+    for (int i = 0; i < 3; i++) {
+        pid[i] = fork();
 
-	if(pid1>0) {
-		pid2 = fork();
-	}
+        if (pid[i] < 0) {
+            printf("Failed to fork\n");
+            exit(1);
+        }
 
-	if(pid1>0 && pid2>0) {
-		waitpid(pid2, &status, 0);
-		printf("parent : child2 - exit(%d)\n", status);
-		waitpid(pid1, &status, 0);
-		printf("parent : child1 - exit(%d)\n", status);
-	}
-	else if(pid1 == 0 && pid2==-1) {
-		sleep(1);
-		exit(1);
-	}
-	else if(pid1>0 && pid2==0) {
-		sleep(2);
-		exit(2);
-	}
-	else printf("fail to fork\n");
+        if (pid[i] == 0) {
+            if (i == 0) {
+                sleep(1);
+                exit(1);
+            } else if (i == 1) {
+                sleep(2);
+                exit(2);
+            } else if (i == 2) {
+                sleep(3);
+                exit(3);
+            }
+        }
+    }
+    if (pid[0] > 0 && pid[1] > 0 && pid[2] > 0) {
+        for (int i = 0; i < 3; i++) {
+            waitpid(pid[i], &status, 0);
+            printf("parent: child%d - exit(%d)\n", i+1, status);
+        }
+    }
+
+    return 0;
 }
