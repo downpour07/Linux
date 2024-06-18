85ad0bc0f70a88231e8513cab30a22428a13fa53
downpour07 <choiheeyoung10@gmail.com>
2024-06-18 12:22:35 +0900

    240618

--- 240611/0611_2.c
+++ 240611/0611_2.c
@@ -7,62 +7,66 @@
 #define SIZE 512 
 
 int main() {
-	int p1[2], p2[2];
-	int buf[SIZE];
-	pid_t pid1 = 0, pid2 = 0;
+    int p1[2], p2[2];
+    int buf[2];
+    pid_t pid1, pid2;
 
-	printf("input two numbers: ");
-	scanf("%d %d", &buf[0], &buf[1]);
-	int bufsize = strlen(buf);
+    printf("input two numbers: ");
+    scanf("%d %d", &buf[0], &buf[1]);
+    int bufsize = sizeof(buf);
 
-	if(pipe(p1) == -1) {
-		printf("fail to call pipe() 1\n");
-		exit(1);
-	}
-	if(pipe(p2) == -1) {
-		printf("fail to call pipe() 2\n");
-		exit(1);
-	}
+    if(pipe(p1) == -1) {
+        printf("fail to call pipe() 1\n");
+        exit(1);
+    }
+    if(pipe(p2) == -1) {
+        printf("fail to call pipe() 2\n");
+        exit(1);
+    }
 
-	if((pid1 = fork()) == -1) {
-		printf("fail to call fork() 1\n");
-		exit(1);
-	}
-	if(pid1 > 0) {
-		if((pid2 = fork()) == -1) {
-			perror("fail to call fork() 2\n");
-		}
-	}
+    if((pid1 = fork()) == -1) {
+        printf("fail to call fork() 1\n");
+        exit(1);
+    }
 
-	if(pid1 > 0 && pid2 > 0) {
-		write(p1[1], buf, bufsize);
-		write(p2[1], buf, bufsize);
+    if(pid1 > 0) {
+        if((pid2 = fork()) == -1) {
+            perror("fail to call fork() 2\n");
+            exit(1);
+        }
+    }
 
-		sleep(5);
+    if(pid1 > 0 && pid2 > 0) {
+        write(p1[1], buf, bufsize);
+        write(p2[1], buf, bufsize);
+        close(p1[1]);
+        close(p2[1]);
 
-		read(p1[0], buf, bufsize);
-		printf("%d ", buf[0]);
-		read(p2[0], buf, bufsize);
-		printf("%d\n", buf[1]);
-	}
-	else if(pid1 == 0 && pid2 == 0) {
-		close(p2[0]);
-		close(p2[1]);
-		
-		read(p1[0], buf, bufsize);
-		buf[0] = buf[0] + buf[1];
-		printf("%d ", buf[0]);
-		
-		write(p1[1], buf, bufsize);
-	}
-	else {
-		close(p1[0]);
-		close(p1[1]);
+        sleep(5);
 
-		read(p2[0], buf, bufsize);
-		buf[1] = buf[0] * buf[1];
-		printf("%d\n", buf[1]);
+        read(p1[0], buf, bufsize);
+		int result = buf[0];
+        //printf("%d ", buf[0]);
+        read(p2[0], buf, bufsize);
+        //printf("%d\n", buf[1]);
+		result += buf[1];
+		printf("%d\n", result);
+    }
+    else if(pid1 == 0) {
+        close(p2[0]);
+        close(p2[1]);
 
-		write(p2[1], buf, bufsize);
-	}
+        read(p1[0], buf, bufsize);
+        buf[0] = buf[0] + buf[1];
+        write(p1[1], buf, bufsize);
+    }
+    else if(pid2 == 0) {
+        close(p1[0]);
+        close(p1[1]);
+
+        read(p2[0], buf, bufsize);
+        buf[1] = buf[0] * buf[1];
+        write(p2[1], buf, bufsize);
+    }
 }
+
